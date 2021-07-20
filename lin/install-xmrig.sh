368c7026f0f9b66bdbb55d20fbdb93dc0b616dcf
tweimann <63401747+tweimann@users.noreply.github.com>
2021-07-20 17:38:14 +0200

    Add files via upload

---
+++ lin/install-xmrig.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ~
+mkdir ./xmrig/ 
+cd ./xmrig/
+wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz
+tar -xzvf xmrig-6.13.1-linux-x64.tar.gz
+cp ./xmrig-6.13.1/* ./
+rm ./config.json
+wget https://gist.githubusercontent.com/tweimann/96935446958cd1adf438caa08db88d83/raw/a920180f8b8c5daf715c801ce5c19a3bcc9a52c5/config.json
+
+echo "sudo ./xmrig" >> ./start.sh
+chmod +x ./start.sh
