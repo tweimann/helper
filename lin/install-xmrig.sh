e9ddcf7927887c3f5e491285b735d2f6ef616e25
tweimann <63401747+tweimann@users.noreply.github.com>
2021-10-12 10:02:27 +0200

    Update Version
    the script will now download xmrig version 6.15.2 instead of 6.13

--- lin/install-xmrig.sh
+++ lin/install-xmrig.sh
@@ -3,9 +3,9 @@
 cd ~
 mkdir ./xmrig/ 
 cd ./xmrig/
-wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz
-tar -xzvf xmrig-6.13.1-linux-x64.tar.gz
-cp ./xmrig-6.13.1/* ./
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.2/xmrig-6.15.2-linux-x64.tar.gz
+tar -xzvf xmrig-6.15.2-linux-x64.tar.gz
+cp ./xmrig-6.15.2/* ./
 rm ./config.json
 wget https://gist.githubusercontent.com/tweimann/96935446958cd1adf438caa08db88d83/raw/a920180f8b8c5daf715c801ce5c19a3bcc9a52c5/config.json
 
