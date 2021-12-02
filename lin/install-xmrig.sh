027cc25aec74d983202a6a38b2efa9aaeea7a52d
tweimann <63401747+tweimann@users.noreply.github.com>
2021-12-02 14:26:43 +0100

    Update install-xmrig.sh

--- lin/install-xmrig.sh
+++ lin/install-xmrig.sh
@@ -7,7 +7,9 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.16.1/xmrig-6.16.1-linux
 tar -xzvf xmrig-6.16.1-linux-x64.tar.gz
 cp ./xmrig-6.16.1/* ./
 rm ./config.json
-wget https://gist.githubusercontent.com/tweimann/96935446958cd1adf438caa08db88d83/raw/a920180f8b8c5daf715c801ce5c19a3bcc9a52c5/config.json
-
+wget https://gist.githubusercontent.com/tweimann/d64abad935e60b192542d38b5684040e/raw/82a714b002e3df910558c90527d94da66f92dfaf/config.json
+wget https://gist.githubusercontent.com/tweimann/cb861d94d3e7a2e76bdf89b4143020e2/raw/ecf0b7e99f8b9efa10c7959559c3add5fd9bb72b/benchmark.sh
+chmod +x ./benchmark.sh
+echo "#!/bin/bash" >> ./start.sh
 echo "sudo ./xmrig" >> ./start.sh
 chmod +x ./start.sh
